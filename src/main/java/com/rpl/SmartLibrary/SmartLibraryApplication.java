25abb544e2054ce34f8a07ceba8091783385e29a
damaabi6 <damaabi647@gmail.com>
2026-09-10 23:36:47 +0800

    Initial commit

---
+++ src/main/java/com/rpl/SmartLibrary/SmartLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.rpl.SmartLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLibraryApplication.class, args);
+	}
+
+}
