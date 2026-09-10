485b7dbcf1ec8e2e726742d64981884d03cb9947
damaabi6 <damaabi647@gmail.com>
2026-09-10 23:55:13 +0800

    lombok

--- src/main/java/com/rpl/SmartLibrary/classes/User.java
+++ src/main/java/com/rpl/SmartLibrary/classes/User.java
@@ -1,35 +1,27 @@
 package com.rpl.SmartLibrary.classes;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class User {
 
-    private String username;
-    private String id;
-    private String password;
+
+    private @Getter String username;
+    private @Getter String id;
+    private @Getter String password;
 
     public User(String username, String id, String password){
         setUsername(username);
-        setid(id);
+        setId(id);
         setPassword(password);
-    }
-
-    public String getUsername() {
-        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getid() {
-        return id;
-    }
-
-    public void setid(String id) {
+    public void setId(String id) {
         this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
